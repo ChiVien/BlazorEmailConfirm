a468c9dfe717465ab965c0d3e7412278b65b0af9
cvtai2409 <71303956+cvtai2409@users.noreply.github.com>
2022-06-16 13:01:15 +0700

    update edit

---
+++ BlazorApp2/Server/Migrations/20220615090138_newMigs.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorApp2.Server.Migrations
+{
+    public partial class newMigs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Mon3",
+                table: "ThiSinh",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Mon2",
+                table: "ThiSinh",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Mon1",
+                table: "ThiSinh",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "HoTen",
+                table: "ThiSinh",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CBTuVan",
+                table: "ThiSinh",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Mon3",
+                table: "ThiSinh",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Mon2",
+                table: "ThiSinh",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Mon1",
+                table: "ThiSinh",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "HoTen",
+                table: "ThiSinh",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CBTuVan",
+                table: "ThiSinh",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
--- BlazorApp2/Shared/ThiSinh.cs
+++ BlazorApp2/Shared/ThiSinh.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,53 +11,102 @@ namespace BlazorApp2.Shared
     public class ThiSinh
     {
         public int Id { get; set; }
+       
+        [MaxLength(50)]
+        [Required(ErrorMessage = "Vui lòng nhập Họ Tên")]
         public string HoTen { get; set; }
-        public string Cmnd { get; set; } = "000000000";
-        public string NgaySinh { get; set; } = "7/7/2022";
+        [Required(ErrorMessage ="Vui lòng nhập CMND")]
+        public string Cmnd { get; set; } 
+        [Required(ErrorMessage = "Vui lòng nhập ngày sinh")]
+        public string NgaySinh { get; set; } 
+        [Required(ErrorMessage = "Vui lòng chọn giới tính")    ]
         public string GioiTinh { get; set; }
+        [Required(ErrorMessage = "Vui lòng chọn bật học")]
         public string BacHoc { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập mã ngành xét tuyển")]
         public string MaNganhXetTuyen { get; set; }
+       
         public string? DoiTuong { get; set; }
-        public int DiemUuTienDT { get; set; } = 1;
+        public int DiemUuTienDT { get; set; } 
+       
         public string? KhuVuc { get; set; }
-        public double DiemUuTienKV { get; set; } = 1;
+      
+        public double DiemUuTienKV { get; set; } 
+        [Required(ErrorMessage ="Vui lòng chọn khối ngành xét tuyển")]
         public string KhoiXetTuyen { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập điểm")]
         public string? Mon1 { get; set; } = "1";
-        public string? Mon2 { get; set; } = "1";
-        public string? Mon3 { get; set; } = "1";
-        public double Diem1111 { get; set; } = 1;
-        public double Diem1211 { get; set; } = 1;
-        public double Diem1112 { get; set; } = 1;
-        public double Diem1212 { get; set; } = 1;
-        public double Diem1TB12 { get; set; } = 1;
-        public double Diem2111 { get; set; } = 1;
-        public double Diem2211 { get; set; } = 1;
-        public double Diem2112 { get; set; } = 1;
-        public double Diem2212 { get; set; } = 1;
-        public double Diem2TB12 { get; set; } = 1;
-        public double Diem3111 { get; set; } = 1;
-        public double Diem3211 { get; set; } = 1;
-        public double Diem3112 { get; set; } = 1;
-        public double Diem3212 { get; set; } = 1;
-        public double Diem3TB12 { get; set; } = 1;
-        public double DiemTBPT1 { get; set; } = 1;
-        public double DiemTBPT2 { get; set; } = 1;
+        [Required(ErrorMessage = "Vui lòng nhập điểm")]
+        public string? Mon2 { get; set; } = "2";
+        [Required(ErrorMessage = "Vui lòng nhập điểm")]
+        public string? Mon3 { get; set; } = "3";
+        [Required(ErrorMessage = "Vui lòng nhập điểm")]
+        public double Diem1111 { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập điểm")]
+        public double Diem1211 { get; set; } 
+        [Required(ErrorMessage = "Vui lòng nhập điểm")]
+        public double Diem1112 { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập điểm")]
+        public double Diem1212 { get; set; } 
+        [Required(ErrorMessage = "Vui lòng nhập điểm")]
+        public double Diem1TB12 { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập điểm")]
+        public double Diem2111 { get; set; } 
+        [Required(ErrorMessage = "Vui lòng nhập điểm")]
+        public double Diem2211 { get; set; } 
+        [Required(ErrorMessage = "Vui lòng nhập điểm")]
+        public double Diem2112 { get; set; } 
+        [Required(ErrorMessage = "Vui lòng nhập điểm")]
+        public double Diem2212 { get; set; } 
+        [Required(ErrorMessage = "Vui lòng nhập điểm")]
+        public double Diem2TB12 { get; set; } 
+        [Required(ErrorMessage = "Vui lòng nhập điểm")]
+        public double Diem3111 { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập điểm")]
+        public double Diem3211 { get; set; } 
+        [Required(ErrorMessage = "Vui lòng nhập điểm")]
+        public double Diem3112 { get; set; } 
+        [Required(ErrorMessage = "Vui lòng nhập điểm")]
+        public double Diem3212 { get; set; } 
+        [Required(ErrorMessage = "Vui lòng nhập điểm")]
+        public double Diem3TB12 { get; set; } 
+        [Required(ErrorMessage = "Vui lòng nhập điểm")]
+        public double DiemTBPT1 { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập điểm")]
+        public double DiemTBPT2 { get; set; } 
+        [Required(ErrorMessage = "Vui lòng nhập điểm")]
         public double DiemTBPT3 { get; set; } = 1;
+   
         public string TruongTHPT { get; set; } = "1";
+       
         public string HK11 { get; set; } = "1";
+       
         public string HK12 { get; set; } = "1";
-        public int? MaDKXT { get; set; } = 1;
-        public double diem1 { get; set; } = 1;
-        public double diem2 { get; set; } = 1;
-        public double diem3 { get; set; } = 1;
-        public double Tong { get; set; } = 1;
-        public string Sdt { get; set; } = "1";
-        public string Email { get; set; } = "1";
+       
+        public int? MaDKXT { get; set; } 
+        [Required(ErrorMessage = "Vui lòng nhập điểm")]
+        public double diem1 { get; set; } 
+        [Required(ErrorMessage = "Vui lòng nhập điểm 1")]
+        public double diem2 { get; set; } 
+        [Required(ErrorMessage = "Vui lòng nhập điểm 2")]
+        public double diem3 { get; set; } 
+        [Required(ErrorMessage ="Vui lòng nhập điểm 3")]
+        public double Tong { get; set; } 
+        [Required(ErrorMessage ="Vui lòng nhập số điện thoại")]
+        public string Sdt { get; set; } 
+        [Required(ErrorMessage ="Vui lòng nhập email")]
+        public string Email { get; set; } 
+        [Required]
         public string Diachi { get; set; } = "1";
+    
         public DateTime Ngaygui { get; set; } = DateTime.Now;
+        [Required]
         public string? CBTuVan { get; set; } = "1";
+        [Required]
         public string TrangThai { get; set; } = "1";
+
         public DateTime CreateDateTime { get; set; } = DateTime.Now;
+       
         public DateTime UpdateDateTime { get; set; } = DateTime.Now;
         public List<HinhAnh> HinhAnhs { get; set; } = new List<HinhAnh>();
     }
